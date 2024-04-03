8a3fcf72d68668e07f8ee9437b284028dcf3bdc2
Ms3314 <hsamiuddin405@gmail.com>
2024-04-03 23:33:06 +0530

    completed the color changer

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -1,9 +1,21 @@
-alert(document.querySelector(".button").getAttribute('class'));
+const clicked = document.querySelectorAll(".button");
+clicked.forEach((t)=> {
+  t.addEventListener('click',function () {
+    const id = t.getAttribute('id')
+    changer(id);
+  })
+})
+// concepts learned
+/*
+-- when using the querySelectorAll please use foreach to loop over each of the members of the nodelist 
+-- getAttribute HELPS U get the attribute that you want which helps u retarget an element 
+
+*/ 
+
 
 function changer(color) {
-  document.getElementById(color).addEventListener('click', function () {
     document.querySelector('body').style.background = color;;
-  });
+  
 }
 
 
