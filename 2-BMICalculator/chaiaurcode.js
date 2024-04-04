2a5776abc71958c66812ee66bdd2b6a871111544
Ms3314 <hsamiuddin405@gmail.com>
2024-04-04 22:36:11 +0530

    mod BMI

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,9 +1,11 @@
 document.querySelector('form').addEventListener('submit', function (l) {
     l.preventDefault();
+    // discontinuing the default function of itself posting a sort of request
     const height = parseInt(document.querySelector("#height").value);
     const weight = parseInt(document.querySelector("#weight").value);
     const submit = document.querySelector("#results")
     if (height === '' || isNaN(height) || height < 0 )
+    // isnan predicts if the given input is a number or not  
     {
         submit.innerHTML = 'please enter a valid height'
     }
@@ -12,13 +14,11 @@ document.querySelector('form').addEventListener('submit', function (l) {
         submit.innerHTML = 'please enter a valid weight'
     }
     else {
-        const ans = (weight/(height*height)).toFixed(2)
-        submit.innerHTML = `<span> ${ans}</span>`
+        const ans = (weight/((height*height)/10000)).toFixed(2)
+        if (ans < 18.6) submit.innerHTML = `<span> you are underweight </span>` 
+        else if (18.6 <= ans < 24.9)  submit.innerHTML = `<span> you are good</span>`
+        else submit.innerHTML = '<span>You are Overweight</span>'
     }
-    
-    
-    
-    
 })
 
 
